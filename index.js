151582f073b8d4c1c8718c472254995d474cd9ef
Couragenwanduka <156565155+Couragenwanduka@users.noreply.github.com>
2024-03-19 08:32:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDB from './config/mongodb.js';
-import router from './routes/routes.js';
+import connectDB from '../config/mongodb.js';
+import router from '../routes/routes.js';
 
 // Load environment variables from .env file
 dotenv.config();
